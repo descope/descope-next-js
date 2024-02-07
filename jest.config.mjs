5685564344ce05a399a99b64130138192fb4317c
Asaf Shen <asaf@descope.com>
2024-02-07 11:10:03 +0200

    coverage

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,9 @@
 import { defaults } from 'jest-config';
 export default {
 	...defaults,
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
 	setupFiles: ['./jest.setup.js'],
 	transform: {
 		'^.+\\.(js|jsx|ts|tsx|mjs)$': [
