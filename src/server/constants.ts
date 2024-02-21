76e6888800465be5a0e52c8407957740e244786d
Asaf Shen <asaf@descope.com>
2024-02-21 17:22:02 +0200

    base headers

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,13 +1,4 @@
-// Replaced in build time
-declare const BUILD_VERSION: string;
-
 export const DESCOPE_SESSION_HEADER = 'x-descope-session';
-
-// eslint-disable-next-line import/prefer-default-export
-export const baseHeaders = {
-	'x-descope-sdk-name': 'nextjs',
-	'x-descope-sdk-version': BUILD_VERSION
-};
 
 export const DEFAULT_PUBLIC_ROUTES = {
 	signIn: process.env.SIGN_IN_ROUTE || '/sign-in',
