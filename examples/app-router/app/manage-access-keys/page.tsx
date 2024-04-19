37a713e2e72f08c738f922b4b7eaaeaa2c67bed7
Doron Sharon <dorsha@descope.com>
2024-04-19 16:18:07 +0300

    Add User Access Key widget to readme (#36)

--- examples/app-router/app/manage-access-keys/page.tsx
+++ examples/app-router/app/manage-access-keys/page.tsx
@@ -15,6 +15,12 @@ export default function ManageAccessKeys() {
 				tenant={process.env.NEXT_PUBLIC_DESCOPE_TENANT}
 				widgetId="access-key-management-widget"
 			/>
+
+			<h1>Manage My Access Keys</h1>
+			<AccessKeyManagement
+				tenant={process.env.NEXT_PUBLIC_DESCOPE_TENANT}
+				widgetId="user-access-key-management-widget"
+			/>
 		</div>
 	);
 }
