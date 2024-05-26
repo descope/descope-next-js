27077806c6f309dfb37d645cedab1110dbd6c944
Nir Gur Arie <nir@descope.com>
2024-05-26 17:55:47 +0300

    update react components

--- examples/app-router/app/layout.tsx
+++ examples/app-router/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
 		<AuthProvider
 			projectId={process.env.NEXT_PUBLIC_DESCOPE_PROJECT_ID}
 			baseUrl={process.env.NEXT_PUBLIC_DESCOPE_BASE_URL}
+			baseStaticUrl={process.env.NEXT_PUBLIC_DESCOPE_BASE_STATIC_URL}
 		>
 			<html lang="en">
 				<body>{children}</body>
